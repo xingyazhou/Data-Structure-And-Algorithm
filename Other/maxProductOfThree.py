5d91cd9623240909dbf9710da7662a00a2e32fc2
xingyazhou <xingyaz@gmail.com>
2020-08-15 22:37:25 +0200

    [ADD] Initial release

---
+++ Other/maxProductOfThree.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 22:07:53 2020
+
+MaxProductOfThree
+Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
+
+Task description
+A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+
+For example, array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+contains the following example triplets:
+
+(0, 1, 2), product is −3 * 1 * 2 = −6
+(1, 2, 4), product is 1 * 2 * 5 = 10
+(2, 4, 5), product is 2 * 5 * 6 = 60
+Your goal is to find the maximal product of any triplet.
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
+
+For example, given array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+
+@author: xingya
+
+"""
+
+
+def MaxProductOfThree(A):
+    
+    if len(A) < 3:
+        raise Exception("Invalid input")
+    
+    A.sort()
+    
+    return max(A[-1]*A[-2]*A[-3], A[-1]*A[0]*A[1])
+               
+               
+A=[-3,1,2,-2,5,6]      
+print(MaxProductOfThree(A))      
+
+# Output
+# 60   
