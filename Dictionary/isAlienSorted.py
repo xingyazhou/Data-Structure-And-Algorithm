a6d1464cb8855a055170e0e1068a1fd5815956f0
xingyazhou <xingyaz@gmail.com>
2020-10-29 21:44:49 +0100

    [ADD] Initial release

--- Dictionary/isAlienSorted.py
+++ Dictionary/isAlienSorted.py
@@ -4,9 +4,11 @@ Created on Thu Oct 29 20:49:31 2020
 953. Verifying an Alien Dictionary (Easy)
 
 Share
-In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the 
+alphabet is some permutation of lowercase letters.
 
-Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given 
+words are sorted lexicographicaly in this alien language.
 
 
 Example 1:
@@ -23,7 +25,8 @@ Example 3:
 
 Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
 Output: false
-Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical 
+rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
 
 @author: xingya
 """
